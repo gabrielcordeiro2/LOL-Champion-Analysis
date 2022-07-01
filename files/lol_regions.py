010c235a23136cee9fd88d8d927c62462dd1280f
gabrielcordeiro2 <gabriel.cordeiro.b.v@gmail.com>
2022-07-01 16:55:06 -0300

    config: add regions file

---
+++ files/lol_regions.py
@@ -0,0 +1,15 @@
+regions = [
+    "Bandle_City",
+    "Bilgewater",
+    "Demacia",
+    "Ionia",
+    "Ixtal",
+    "Noxus",
+    "Piltover",
+    "Shadow_Isles",
+    "Shurima",
+    "Targon",
+    "Freljord",
+    "Void",
+    "Zaun"
+]
